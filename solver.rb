24b8a0174abca4252676bda05ec58f539c4b6f75
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-03 18:22:25 +0300

    Create factorial(num) method inside Solver class

--- solver.rb
+++ solver.rb
@@ -1,4 +1,15 @@
 class Solver
-  
+  def factorial(num)
+    if num.zero?
+      return 1
+    elsif num == 1
+      return num
+    elsif num.negative?
+      raise 'Argument cannot be negative'
+    end
+
+    num * factorial(num - 1)
+  end
+
 end
 
