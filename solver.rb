be70f96d27f88f2f5368b613635d011c78ae6c62
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-08-03 12:26:47 -0300

    Create fizzbuzz method in Solver class

--- solver.rb
+++ solver.rb
@@ -16,4 +16,16 @@ class Solver
 
     word.reverse
   end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
 end
